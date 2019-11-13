ebc9993efbd1c74a39a207cd4e51758334c70363
zzzonki <zzzonki@gmail.com>
2019-11-14 00:47:54 +0300

    урок 9

--- main.js
+++ main.js
@@ -13,7 +13,22 @@ let cross = document.querySelectorAll('.cookie__exit')
 let promo_form = document.querySelector('.form_wrapper')
 let action__btn = document.querySelector('.action__btm')
 let def_animation = 'fadeOut'
-let card__line = document.querySelector('.card__line')
+let card__lines = document.querySelectorAll('.card__line')
+let faq_quest = document.querySelectorAll('.faq_panel__quest')
+let faq_answer = document.querySelectorAll('.faq_panel__answer')
+
+for (let i = 0; i < card__lines.length; i++) {
+    card__lines[i].onclick = function(){
+        this.classList.toggle('card__line__helper')
+    }
+    // alert(i*i)
+}
+
+// let cat = {
+//     color: "grey"
+//     style: "vintage"
+// }
+// console.log(cat.color)
 
 cross[1].onclick = () => {
     cookie.style.display = "none"
@@ -43,10 +58,44 @@ cross[0].onclick = () => {
 // Есть красные линии на карточках
 // При наведении они становятся шире
 
-card__line.onmouseover = () => {
-    card__line.style.width = "150px"
-    // alert("по мне кликнули")
-}
-card__line.onmouseout = () => {
-    card__line.style.width = "90px"
+// card__lines.onmouseover = () => {
+//     card__line.style.width = "150px"
+//     // alert("по мне кликнули")
+// }
+// card__lines.onmouseout = () => {
+//     card__line.style.width = "90px"
+// }
+
+// При клике на вопросы открываются ответы
+
+var flag
+flag = true
+
+for (let i = 0; i < faq_quest.length; i++) {
+    faq_quest[i].onclick = function(){
+        faq_answer[i].classList.add('animated')
+        if(flag){
+            faq_answer[i].classList.remove('zoomOut')
+            faq_answer[i].classList.add('zoomIn')
+            faq_answer[i].classList.toggle('faq__helper')
+            flag = false
+        }
+        else{
+            faq_answer[i].classList.remove('zoomIn')
+            faq_answer[i].classList.add('zoomOut')
+            flag = true
+            setTimeout(() => {
+                faq_answer[i].classList.remove('faq__helper')
+            }, 400)
+        }
+
+        // if(faq_answer[i].classList.contains('zoomOut')){
+        //     faq_answer[i].classList.remove('zoomOut'))
+        //     faq_answer[i].classList.add('zoomIn')
+        // }
+        // if(faq_answer[i].classList.contains('zoomIn')){
+        //     faq_answer[i].classList.remove('zoomIn')
+        //     faq_answer[i].classList.add('zoomOut')
+        // }
+    }
 }
