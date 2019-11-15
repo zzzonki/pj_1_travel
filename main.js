421c7b396be74b82e922d899a56023b4668702e1
zzzonki <zzzonki@gmail.com>
2019-11-15 05:56:22 +0300

    Куки с сюрпризом

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ let def_animation = 'fadeOut'
 let card__lines = document.querySelectorAll('.card__line')
 let faq_quest = document.querySelectorAll('.faq_panel__quest')
 let faq_answer = document.querySelectorAll('.faq_panel__answer')
+let cookie__button = document.querySelector('.cookie__button')
 
 for (let i = 0; i < card__lines.length; i++) {
     card__lines[i].onclick = function(){
@@ -30,9 +31,8 @@ for (let i = 0; i < card__lines.length; i++) {
 // }
 // console.log(cat.color)
 
-cross[1].onclick = () => {
+cookie__button.onclick = () => {
     cookie.style.display = "none"
-    // ck_warning.remove()
 }
 
 // у нас есть форма, которая изначально не видна
@@ -75,18 +75,18 @@ for (let i = 0; i < faq_quest.length; i++) {
     faq_quest[i].onclick = function(){
         faq_answer[i].classList.add('animated')
         if(flag){
-            faq_answer[i].classList.remove('zoomOut')
-            faq_answer[i].classList.add('zoomIn')
+            faq_answer[i].classList.remove('flipOutX')
+            faq_answer[i].classList.add('flipInX')
             faq_answer[i].classList.toggle('faq__helper')
             flag = false
         }
         else{
-            faq_answer[i].classList.remove('zoomIn')
-            faq_answer[i].classList.add('zoomOut')
+            faq_answer[i].classList.remove('flipInX')
+            faq_answer[i].classList.add('flipOutX')
             flag = true
             setTimeout(() => {
                 faq_answer[i].classList.remove('faq__helper')
-            }, 400)
+            }, 400) 
         }
 
         // if(faq_answer[i].classList.contains('zoomOut')){
@@ -98,4 +98,32 @@ for (let i = 0; i < faq_quest.length; i++) {
         //     faq_answer[i].classList.add('zoomOut')
         // }
     }
+}
+// Печенька на куки-панели меняется на обведенную печеньку и обратно, при нажатии. Пати хард.
+
+let cookie_logo = document.querySelector('.cookie__logo')
+
+cookie_logo.onclick = function(){
+    this.classList.toggle('cookie__logo_helper')
+}
+
+// При нажатии на крест на куки-панели появляется несогласный куки-монстр посреди экрана
+// Чтобы убрать панель теперь нужно нажать кнопку "Я понял"
+// Чтобы убрать монстра, нужно кликнуть на него
+
+let screamer = document.querySelector('.cookie__screamer')
+
+cross[1].onclick = () => {
+    screamer.classList.remove('zoomOutDown')
+    screamer.style.display = 'block'
+    screamer.classList.add('animated')
+    screamer.classList.add('zoomInUp')
+}
+
+screamer.onclick = function() {
+    this.classList.add('zoomOutDown')
+    setTimeout(() => {
+        screamer.style.display = 'none'
+    }, 600) 
+    // this.classList.remove('screamer')
 }
